5a947a95a95e9a3cc2b4da933695ce25fee8d424
Tatiana Kh <ts.khol@gmail.com>
2023-07-02 17:42:26 +0300

    refactor with ESM

---
+++ index.js
@@ -0,0 +1,71 @@
+import slider from './slider.js'
+
+const options = {
+  root: '#slider',
+  width: 750,
+  height: 400,
+  slides: [
+    {
+      color: '#c62828',
+      text: 'RED'
+    },
+    {
+      color: '#ad1457',
+      text: 'PINK'
+    },
+    {
+      color: '#6a1b9a',
+      text: 'PURPLE'
+    },
+    {
+      color: '#4527a0',
+      text: 'DEEP_PURPLE'
+    },
+    {
+      color: '#283593',
+      text: 'INDIGO'
+    },
+    {
+      color: '#1565c0',
+      text: 'BLUE'
+    },
+    {
+      color: '#0277bd',
+      text: 'LIGHT_BLUE'
+    },
+    {
+      color: '#00838f',
+      text: 'CYAN'
+    },
+    {
+      color: '#00695c',
+      text: 'TEAL'
+    },
+    {
+      color: '#2e7d32',
+      text: 'GREEN'
+    },
+    {
+      color: '#558b2f',
+      text: 'LIGHT_GREEN'
+    },
+    {
+      color: '#827717',
+      text: 'LIME'
+    },
+    {
+      color: '#ef6c00',
+      text: 'ORANGE'
+    },
+    {
+      color: '#d84315',
+      text: 'DEEP_ORANGE'
+    },
+    {
+      color: '#4e342e',
+      text: 'BROWN'
+    }
+  ]
+}
+
+slider(options)
--- slider.js
+++ slider.js
@@ -1,3 +1,10 @@
+export default function slider(options) {
+  validate(options)
+  initSlider(options)
+  insertSlides(options)
+  swipeSlides(options)
+}
+
 function swipeSlide(index) {
   const slider = document.querySelector('.slider')
   const shift = -750 * index
@@ -37,80 +44,3 @@ function initSlider(options) {
   wrapper.style.width = `${width}px`
   wrapper.style.height = `${height}px`
 }
-
-function slider(options) {
-  validate(options)
-  initSlider(options)
-  insertSlides(options)
-  swipeSlides(options)
-}
-
-const options = {
-  root: '#slider',
-  width: 750,
-  height: 400,
-  slides: [
-    {
-      color: '#c62828',
-      text: 'RED'
-    },
-    {
-      color: '#ad1457',
-      text: 'PINK'
-    },
-    {
-      color: '#6a1b9a',
-      text: 'PURPLE'
-    },
-    {
-      color: '#4527a0',
-      text: 'DEEP_PURPLE'
-    },
-    {
-      color: '#283593',
-      text: 'INDIGO'
-    },
-    {
-      color: '#1565c0',
-      text: 'BLUE'
-    },
-    {
-      color: '#0277bd',
-      text: 'LIGHT_BLUE'
-    },
-    {
-      color: '#00838f',
-      text: 'CYAN'
-    },
-    {
-      color: '#00695c',
-      text: 'TEAL'
-    },
-    {
-      color: '#2e7d32',
-      text: 'GREEN'
-    },
-    {
-      color: '#558b2f',
-      text: 'LIGHT_GREEN'
-    },
-    {
-      color: '#827717',
-      text: 'LIME'
-    },
-    {
-      color: '#ef6c00',
-      text: 'ORANGE'
-    },
-    {
-      color: '#d84315',
-      text: 'DEEP_ORANGE'
-    },
-    {
-      color: '#4e342e',
-      text: 'BROWN'
-    }
-  ]
-}
-
-slider(options)
