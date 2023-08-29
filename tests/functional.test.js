575a442c214f69f664a1d25b731a8c008b2cf3bf
Tatiana Kh <ts.khol@gmail.com>
2023-08-29 23:42:29 +0300

    update jsdom

--- tests/functional.test.js
+++ tests/functional.test.js
@@ -1,9 +1,10 @@
 import { expect, test } from 'vitest'
 import slider from './slider.js'
+import { JSDOM } from 'jsdom'
 
 test('throws an exception when data is invalid', () => {
   const options = {
-    root: null,
+    root: undefined,
     width: 750,
     height: 400,
     slides: [
@@ -47,4 +48,30 @@ test('executes without an error', () => {
   }
 
   expect(slider(options)).toBe(null)
+})
+
+const expect = require("chai").expect
+const { JSDOM } = require("jsdom")
+
+describe("updateMsg", function() {
+  let jsdom
+  before(async function() {
+    jsdom = await JSDOM.fromFile("index.html", {
+      resources: "usable",
+      runScripts: "dangerously"
+    })
+    await new Promise(resolve =>
+      jsdom.window.addEventListener("load", resolve)
+    )
+  })
+
+  it('updates the innerHTML of element with id "msg"', function() {
+    expect(jsdom.window.document.getElementById("msg").innerHTML).to.equal(
+      "Hello, World!"
+    )
+    jsdom.window.updateMsg("The new msg!")
+    expect(jsdom.window.document.getElementById("msg").innerHTML).to.equal(
+      "The new msg!"
+    )
+  })
 })
